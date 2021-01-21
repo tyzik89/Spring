6a97a7b16c3f170d8d76e0d62052d0e02f933679
Владимир <work.vladimir.s@gmail.com>
2021-01-21 23:07:02 +0300

    Initial commit

---
+++ telescopes-cloud/src/main/java/com/vladimirs/work/telescopescloud/TelescopesCloudApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirs.work.telescopescloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelescopesCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelescopesCloudApplication.class, args);
+    }
+
+}
---
+++ telescopes-cloud/src/test/java/com/vladimirs/work/telescopescloud/TelescopesCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirs.work.telescopescloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelescopesCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
