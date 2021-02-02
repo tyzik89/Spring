0d7f1e0b3927a8ab3d89dec4bc43cb527572e7c6
Vladimir <work.vladimir.s@gmail.com>
2021-02-03 00:21:10 +0300

    Fix

--- rockets-cloud/src/main/java/com/work/vladimirs/rocketscloud/data/repositories/ComponentRepositoryJdbcImpl.java
+++ rockets-cloud/src/main/java/com/work/vladimirs/rocketscloud/data/repositories/ComponentRepositoryJdbcImpl.java
@@ -1,12 +1,14 @@
 package com.work.vladimirs.rocketscloud.data.repositories;
 
 import com.work.vladimirs.rocketscloud.models.inventory.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ComponentRepositoryJdbcImpl implements ComponentRepository {
 
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
